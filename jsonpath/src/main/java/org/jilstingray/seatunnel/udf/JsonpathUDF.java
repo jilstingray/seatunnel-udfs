9cf968ac7ecd4a5a303835c04ec7083c440f7a5b
Stingray <35288750+jilstingray@users.noreply.github.com>
2023-10-28 19:34:53 +0800

    Bug fix

--- jsonpath/src/main/java/org/jilstingray/seatunnel/udf/JsonpathUDF.java
+++ jsonpath/src/main/java/org/jilstingray/seatunnel/udf/JsonpathUDF.java
@@ -18,32 +18,35 @@
 package org.jilstingray.seatunnel.udf;
 
 import com.google.auto.service.AutoService;
+import com.jayway.jsonpath.JsonPath;
 import org.apache.seatunnel.api.table.type.BasicType;
 import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
-import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 import org.apache.seatunnel.transform.sql.zeta.ZetaUDF;
 
 import java.util.List;
 
 @AutoService(ZetaUDF.class)
-public class JsonPath implements ZetaUDF {
+public class JsonpathUDF implements ZetaUDF {
 
     @Override
     public String functionName() {
-        return "JSON_PATH";
+        return "JSONPATH";
     }
 
     @Override
-    public SeaTunnelDataType<?> resultType(List<SeaTunnelDataType<?>> list) {
+    public SeaTunnelDataType<?> resultType(List<SeaTunnelDataType<?>> argsType) {
         return BasicType.STRING_TYPE;
     }
 
     @Override
     public Object evaluate(List<Object> args) {
-        SeaTunnelRow row = (SeaTunnelRow) args.get(0);
-        String json = (String) row.getField(0);
+        String json = (String) args.get(0);
         String path = (String) args.get(1);
-        List<String> values = com.jayway.jsonpath.JsonPath.read(json, path);
-        return values.get(0);
+        Object value = JsonPath.read(json, path);
+        if (value.getClass().isArray()) {
+            return ((Object[]) value)[0];
+        } else {
+            return value;
+        }
     }
 }
