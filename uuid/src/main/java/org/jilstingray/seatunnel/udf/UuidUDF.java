857ce082770582b9537c0ba7d924ffc0e9a46ca9
Stingray <35288750+jilstingray@users.noreply.github.com>
2023-11-09 16:51:56 +0800

    UUID

---
+++ uuid/src/main/java/org/jilstingray/seatunnel/udf/UuidUDF.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jilstingray.seatunnel.udf;
+
+import com.google.auto.service.AutoService;
+import org.apache.seatunnel.api.table.type.BasicType;
+import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
+import org.apache.seatunnel.transform.sql.zeta.ZetaUDF;
+
+import java.util.List;
+import java.util.UUID;
+
+@AutoService(ZetaUDF.class)
+public class UuidUDF implements ZetaUDF {
+    @Override
+    public String functionName() {
+        return "UUID";
+    }
+
+    @Override
+    public SeaTunnelDataType<?> resultType(List<SeaTunnelDataType<?>> argsType) {
+        return BasicType.STRING_TYPE;
+    }
+
+    @Override
+    public Object evaluate(List<Object> args) {
+        return UUID.randomUUID().toString();
+    }
+}
